bc102ebcd13fd7574eef2c9d5de69453cb4dff43
pghewari <pghewari@onenetwork.com>
2017-01-27 20:01:37 +0530

    First java class for checking GetHub repository... 27th Jan 2017

---
+++ src/com/prashant/corejava/HelloJava.java
@@ -0,0 +1,10 @@
+package com.prashant.corejava;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First java class for checking GetHub repository... 27th Jan 2017");
+	}
+
+}
